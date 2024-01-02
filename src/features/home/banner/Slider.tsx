96c0ca49c226e6501a517ab15b42c5079509395e
Mohammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2024-01-02 23:11:11 +0600

    embla-carousel-autoplay package installed and configure with the carousel and carousel dragFree property disabled

--- src/features/home/banner/Slider.tsx
+++ src/features/home/banner/Slider.tsx
@@ -1,44 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Carousel } from "@mantine/carousel";
+import Autoplay from "embla-carousel-autoplay";
 import { Box, Image, Paper, rem } from "@mantine/core";
 import QuickBusinessWizardForm from "./QuickBannerWizerdForm";
 
 const sliderImages = [
-  "/images/slider-1.jpg",
-  "/images/slider-2.jpeg",
-  "/images/slider-3.jpeg"
+    "/images/slider-1.jpg",
+    "/images/slider-2.jpeg",
+    "/images/slider-3.jpeg",
 ];
 
 const TopSlider = () => {
-  return (
-    <Box pos="relative">
-      <Carousel
-        withControls={false}
-        withIndicators
-        loop
-        height={`calc(100vh - ${rem(100)})`}
-        dragFree
-        slideGap="md"
-        align="start"
-      >
-        {sliderImages.map((slider, idx) => (
-          <Carousel.Slide key={idx}>
-            <Image src={slider} alt={`slider image - ${idx + 1}`} />
-          </Carousel.Slide>
-        ))}
+    const autoplay = useRef(Autoplay({ delay: 3000 }));
+    return (
+        <Box pos="relative">
+            <Carousel
+                withControls={false}
+                withIndicators
+                loop
+                height={`calc(100vh - ${rem(100)})`}
+                // dragFree
+                slideGap="md"
+                align="start"
+                plugins={[autoplay.current]}
+            >
+                {sliderImages.map((slider, idx) => (
+                    <Carousel.Slide key={idx}>
+                        <Image src={slider} alt={`slider image - ${idx + 1}`} />
+                    </Carousel.Slide>
+                ))}
 
-        {/* ...other slides */}
-      </Carousel>
+                {/* ...other slides */}
+            </Carousel>
 
-      <Paper
-        shadow="sm"
-        radius={"md"}
-        className="absolute -bottom-5 right-5 overflow-hidden"
-      >
-        <QuickBusinessWizardForm />
-      </Paper>
-    </Box>
-  );
+            <Paper
+                shadow="sm"
+                radius={"md"}
+                className="absolute -bottom-5 right-5 overflow-hidden"
+            >
+                <QuickBusinessWizardForm />
+            </Paper>
+        </Box>
+    );
 };
 
 export default TopSlider;
